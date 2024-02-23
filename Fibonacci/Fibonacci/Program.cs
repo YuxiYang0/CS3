c15210c424999659db8ca051ff728e1d1023d059
Yuxi Yang <yy6znb@virginia.edu>
2024-02-22 21:53:48 -0500

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 2)
+        {
+            return 1;
+        }
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
